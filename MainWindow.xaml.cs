49c9c0972a18f6e464ea53f9f01bf4970ffccd2a
evelinazhitkova2@mail.ru <evelinazhitkova2@mail.ru>
2022-09-27 11:04:45 +1000

    2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,11 +23,26 @@ namespace WPFFILIPPOV_PR2
         public MainWindow()
         {
             InitializeComponent();
+            X.Text = "5,5";
+            Y.Text = "4,3";
+            Z.Text = "2,8";
+            Itog.Text = "Ст. гр. ИСП Филиппов А.А.";
         }
 
         private void Schet_Click(object sender, RoutedEventArgs e)
         {
-
+            Itog.Text += Environment.NewLine + "Практическая работа 2";
+            double x = double.Parse(X.Text);
+            Itog.Text += Environment.NewLine + "X =" + x.ToString();
+            double y = double.Parse(Y.Text);
+            Itog.Text += Environment.NewLine + "Y =" + y.ToString();
+            double z = double.Parse(Z.Text);
+            Itog.Text += Environment.NewLine + "Z =" + z.ToString();
+            double a = Math.Tan(x + y) * Math.Tan(x + y);
+            double b = Math.Exp(y - z);
+            double c = Math.Sqrt(Math.Cos(x * x) + Math.Sin(z * z));
+            double u = a - b * c;
+            Itog.Text += Environment.NewLine + "Результат U = " + u.ToString();
         }
     }
 }
